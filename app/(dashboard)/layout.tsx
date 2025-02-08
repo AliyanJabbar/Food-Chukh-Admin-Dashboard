0a33de0b66673e577b252f44c3015175eea63d91
aliyan jabbar <jabbaraliyan805@gmail.com>
2025-02-09 01:34:36 +0500

    adding sanity configuration

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -26,11 +26,11 @@ import {
   TooltipTrigger
 } from '@/components/ui/tooltip';
 import { Analytics } from '@vercel/analytics/react';
-import  User  from './user';
-import { VercelLogo } from '@/components/icons';
+import User from './user';
 import Providers from './providers';
 import { NavItem } from './nav-item';
 import { SearchInput } from './search';
+import Image from 'next/image';
 
 export default function DashboardLayout({
   children
@@ -63,11 +63,16 @@ function DesktopNav() {
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <Link
-          href="https://vercel.com/templates/next.js/admin-dashboard-tailwind-postgres-react-nextjs"
+          href="/"
           className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
-          <VercelLogo className="h-3 w-3 transition-all group-hover:scale-110" />
-          <span className="sr-only">Acme Inc</span>
+          <Image
+            src="/android-chrome-192x192.png"
+            alt="logo"
+            width={50}
+            height={50}
+            className="rounded-md"
+          />
         </Link>
 
         <NavItem href="#" label="Dashboard">
